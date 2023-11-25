0388c2b349bafb587c3cbbeb00655d17f10fba28
Yu0614 <azu369yu@gmail.com>
2023-11-26 01:19:57 +0900

    feat: リポジトリ層のベース追加

---
+++ lib/repository/base.dart
@@ -0,0 +1,14 @@
+import '../models/result/result.dart';
+
+abstract class RepositoryBase<T> {
+  Future<Result<List<T>>> fetch({
+    String? where,
+    List? whereArgs,
+  });
+
+  Future<Result<int>> save(T model);
+
+  Future<Result<int>> update(T model);
+
+  Future<Result<int>> delete(T model);
+}
