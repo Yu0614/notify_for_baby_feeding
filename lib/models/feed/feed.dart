ed6b73e885f16dabd092dc26c3c549debc1f40a4
Yu0614 <azu369yu@gmail.com>
2023-11-26 01:18:53 +0900

    feat: feedモデル追加

---
+++ lib/models/feed/feed.dart
@@ -0,0 +1,34 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'feed.freezed.dart';
+part 'feed.g.dart';
+
+@freezed
+class FeedModel with _$FeedModel {
+  const factory FeedModel({
+    /// id
+    int? id,
+
+    /// feedの内容
+    @Default('') String? memo,
+
+    // feedの量
+    @JsonKey(name: 'amount') @Default(0) int? amount,
+
+    /// feedを行う日付 UnixTime
+    @JsonKey(name: 'feed_at') @Default(0) DateTime? feedAt,
+
+    /// 作成日 UnixTime
+    @JsonKey(name: 'created_at') @Default(0) DateTime? createdAt,
+
+    /// 更新日 UnixTime
+    @JsonKey(name: 'updated_at') @Default(0) DateTime? updatedAt,
+  }) = _FeedModel;
+
+  const FeedModel._();
+
+  @override
+  factory FeedModel.fromJson(Map<String, dynamic> json) =>
+      _$FeedModelFromJson(json);
+  // String get formatDate => DateFormat('yyyy-MM-dd').format(dateTime);
+}
---
+++ lib/models/feed/feed.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'feed.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$FeedModelImpl _$$FeedModelImplFromJson(Map<String, dynamic> json) =>
+    _$FeedModelImpl(
+      id: json['id'] as int?,
+      memo: json['memo'] as String? ?? '',
+      amount: json['amount'] as int? ?? 0,
+      feedAt: json['feed_at'] == null
+          ? 0
+          : DateTime.parse(json['feed_at'] as String),
+      createdAt: json['created_at'] == null
+          ? 0
+          : DateTime.parse(json['created_at'] as String),
+      updatedAt: json['updated_at'] == null
+          ? 0
+          : DateTime.parse(json['updated_at'] as String),
+    );
+
+Map<String, dynamic> _$$FeedModelImplToJson(_$FeedModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'memo': instance.memo,
+      'amount': instance.amount,
+      'feed_at': instance.feedAt?.toIso8601String(),
+      'created_at': instance.createdAt?.toIso8601String(),
+      'updated_at': instance.updatedAt?.toIso8601String(),
+    };
