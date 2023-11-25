34b0833d89fb2e0958c1700c4000553f7c6e9a4c
Yu0614 <azu369yu@gmail.com>
2023-11-26 01:20:23 +0900

    feat: リポジトリ層追加

---
+++ lib/repository/feed_repository.dart
@@ -0,0 +1,49 @@
+import '../db/feeds.dart';
+import '../models/feed/feed.dart';
+import '../models/result/result.dart';
+import 'base.dart';
+
+class FeedRepositoryImpl implements RepositoryBase<FeedModel> {
+  FeedRepositoryImpl();
+
+  /// [where]は id = ? のような形式にする
+  @override
+  Future<Result<List<FeedModel>>> fetch({
+    String? where,
+    List? whereArgs,
+  }) async {
+    return Result.guardFuture(() async {
+      final result = await FeedsDBAccessor.db.get(
+        where: where,
+        whereArgs: whereArgs,
+      );
+      return result.map((e) => FeedModel.fromJson(e)).toList();
+    });
+  }
+
+  @override
+  Future<Result<int>> save(FeedModel feed) async {
+    return Result.guardFuture(
+      () async => await FeedsDBAccessor.db.create(json: feed.toJson()),
+    );
+  }
+
+  @override
+  Future<Result<int>> update(FeedModel feed) async {
+    return Result.guardFuture(
+      () async => await FeedsDBAccessor.db.update(
+        json: feed.toJson(),
+        primaryKey: feed.id!,
+      ),
+    );
+  }
+
+  @override
+  Future<Result<int>> delete(FeedModel feed) async {
+    return Result.guardFuture(
+      () async => await FeedsDBAccessor.db.delete(
+        primaryKey: feed.id!,
+      ),
+    );
+  }
+}
