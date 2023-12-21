82d5cb73272fecb0773a1fee567e553cbe849f80
Yu0614 <azu369yu@gmail.com>
2023-12-22 00:33:05 +0900

    feat: アプリのバージョンを表示

--- lib/src/feature/settings/screen/setting.dart
+++ lib/src/feature/settings/screen/setting.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends HookWidget {
   const SettingsPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(
+    BuildContext context,
+  ) {
+    final fromPlatform = useMemoized(PackageInfo.fromPlatform);
+    final snapshot = useFuture(fromPlatform);
+    if (!snapshot.hasData) {
+      return const SizedBox.shrink();
+    }
     return Center(
         child: Column(
       children: [
@@ -15,10 +24,10 @@ class SettingsPage extends StatelessWidget {
             SettingsSection(
               title: const Padding(
                 padding: EdgeInsets.only(
-                    top: 50, //上４
-                    left: 10, //左８
-                    right: 10, //右８
-                    bottom: 10 //下４
+                    top: 50,
+                    left: 5,
+                    right: 10,
+                    bottom: 20
                     ),
                 child: Align(
                   alignment: Alignment.centerLeft,
@@ -28,11 +37,10 @@ class SettingsPage extends StatelessWidget {
                 ),
               ),
               tiles: <SettingsTile>[
-                SettingsTile.navigation(
-                  leading: const Icon(Icons.language),
-                  title: const Text('Language'),
-                  value: const Text('日本語'),
-                ),
+                SettingsTile(
+                    leading: const Icon(Icons.info),
+                    title: const Text('アプリのバージョン'),
+                    value: Text("${snapshot.data?.version}"))
               ],
             ),
           ],
