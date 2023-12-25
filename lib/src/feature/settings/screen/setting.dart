0eb2026970ad13219337aefc226e0b5dff823fba
Yu0614 <azu369yu@gmail.com>
2023-12-26 01:07:51 +0900

    feat: ミルク通知ON/OFF用のトグルを用意 & セクションを分ける

--- lib/src/feature/settings/screen/setting.dart
+++ lib/src/feature/settings/screen/setting.dart
@@ -23,26 +23,41 @@ class SettingsPage extends HookWidget {
           sections: [
             SettingsSection(
               title: const Padding(
-                padding: EdgeInsets.only(
-                    top: 50,
-                    left: 5,
-                    right: 10,
-                    bottom: 20
-                    ),
+                padding:
+                    EdgeInsets.only(top: 50, left: 0, right: 10, bottom: 20),
                 child: Align(
                   alignment: Alignment.centerLeft,
-                  child: Text("設定",
+                  child: Text("アプリについて",
                       style:
                           TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
                 ),
               ),
               tiles: <SettingsTile>[
                 SettingsTile(
-                    leading: const Icon(Icons.info),
+                    leading: const Icon(Icons.info_sharp),
                     title: const Text('アプリのバージョン'),
                     value: Text("${snapshot.data?.version}"))
               ],
             ),
+            SettingsSection(
+              title: const Padding(
+                padding: EdgeInsets.only(top: 0, left: 0, right: 0, bottom: 1),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text("通知設定",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 15)),
+                ),
+              ),
+              tiles: <SettingsTile>[
+                SettingsTile.switchTile(
+                  leading: const Icon(Icons.notifications_sharp),
+                  title: const Text('ミルクを飲む時間に通知する'),
+                  initialValue: false,
+                  onToggle: (value) {},
+                )
+              ],
+            )
           ],
         ) // constrain height
             )
